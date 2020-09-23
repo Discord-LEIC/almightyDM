c05fb7a43808ca4b2a2c6241bb1804d6490a9915
TiagoFonseca99 <47889514+TiagoFonseca99@users.noreply.github.com>
2020-09-23 16:03:38 +0100

    Rss fully working on all channels

--- rss.js
+++ rss.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const courses = require('./courses.json')[1];
+const courses = require('./courses.json');
 
 var CronJob = require('cron').CronJob;
 let Parser = require('rss-parser');
@@ -8,6 +8,8 @@ let parser = new Parser();
 const testing_id = '757019523252748351';
 
 var guild;
+var currentSemester = "1";
+var tmpYear = 0;
 
 function get_channel(id) {
     return guild.channels.cache.get(id);
@@ -49,14 +51,35 @@ function strip_html(message) {
     return message.replace(/<[^<]+?>|\\xa0/gi, '')
 }
 
-function format_feed_entry(entry) {
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+        hour = d.getHours();
+        min = d.getMinutes();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+    if (hour.toString().length < 2) 
+        hour = '0' + hour;
+    if (min.toString().length < 2) 
+        min = '0' + min;
+    
+    return `_${day}-${month}-${year} ${hour}:${min}_\n\n`;
+}
+
+function format_feed_entry(course, entry) {
 
     const embed = new Discord.MessageEmbed()
-        .setTitle(strip_html(entry.title))
+        .setTitle(`[${course.acronym}] ${strip_html(entry.title)}`)
         //.setColor(course.color)
-        .setDescription(strip_html(entry.content.substring(0, 2000)))
-        .setAuthor(entry.author.replace(/(.*@.* \(|\))/gi, ''))
-        .addField('Anúncio Original', `[Clica aqui](${entry.link})`, false)
+        .setDescription(formatDate(entry.pubDate) + strip_html(entry.content.substring(0, 2000)))
+        .setURL(entry.guid)
+        .setAuthor(entry.author.replace(/(.*@.* \(|\))/gi, ''), `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`)
+        .addField('Anúncio Original', `[Clica aqui](${entry.guid})`, false)
 
     //check_embed_len(embed, ' (...)')
 
@@ -66,21 +89,31 @@ function format_feed_entry(entry) {
 function start(guildServer) {
     guild = guildServer;
 
-    var job = new CronJob('*/5 * * * * *', async () => {
+    var job = new CronJob('* */3 * * * *', async () => {
 
-        for (key in courses) {
-            course = courses[key];
-
-            if (course.discord_id !== '') {
-                let feed = await parser.parseURL(course.announcements);
-
-                let channel = get_channel(course.discord_id);
-            
-                feed.items.forEach(item => {
-                    channel.send(format_feed_entry(item));
-                });
+        for (key in courses[(tmpYear+1).toString()][currentSemester]) {
+            course = courses[(tmpYear+1).toString()][currentSemester][key];
+            let feed = await parser.parseURL(course.announcements);
+            feed.items.sort((a, b) => {
+                return (new Date(b.pubDate)).getTime() - (new Date(a.pubDate)).getTime();
+            });
+                
+            let channel = get_channel(course.discord_id);
+                
+            let index = feed.items.length; 
+            await channel.messages.fetch({ limit: 1 }).then(messages => {
+                if (messages.first() !== undefined) {
+                    let lastMessageUrl = messages.first().embeds[0].url;
+                    index = feed.items.findIndex(item => { return item.guid.toString() === lastMessageUrl.toString(); });    
+                }
+            })
+            .catch(console.error);
+                
+            for (let i = index - 1; i >= 0; i--) {
+                channel.send(format_feed_entry(course, feed.items[i]));
             }
         }
+        tmpYear = (tmpYear + 1) % 3;
 
     }, null, true);
 
